2ff624e3332b3dd6da99add3cbec00843ae6b5da
Kevin Schrader <kevinschrader@gmx.de>
2017-05-01 06:12:40 +0200

    optimized update.php, added basic dynamic querying, added db update button

--- index.php
+++ index.php
@@ -1,9 +1,117 @@
 <head>
 
-<script type="text/javascript" src="//wow.zamimg.com/widgets/power.js"></script><script>var wowhead_tooltips = { "colorlinks": true, "iconizelinks": true, "renamelinks": true }</script>
+<script type="text/javascript" src="//wow.zamimg.com/widgets/power.js"></script>
+<script>var wowhead_tooltips = { "colorlinks": true, "iconizelinks": true, "renamelinks": true }</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<style type="text/css">
+    /* The Modal (background) */
+    .modal {
+        display: none; /* Hidden by default */
+        position: fixed; /* Stay in place */
+        z-index: 1; /* Sit on top */
+        left: 0;
+        top: 0;
+        width: 100%; /* Full width */
+        height: 100%; /* Full height */
+        overflow: auto; /* Enable scroll if needed */
+        background-color: rgb(0,0,0); /* Fallback color */
+        background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+    }
 
+    /* Modal Content/Box */
+    .modal-content {
+        background-color: #fefefe;
+        margin: 15% auto; /* 15% from the top and centered */
+        padding: 20px;
+        border: 1px solid #888;
+        width: 80%; /* Could be more or less, depending on screen size */
+    }
 
+    /* The Close Button */
+    .close {
+        color: #aaa;
+        float: right;
+        font-size: 28px;
+        font-weight: bold;
+    }
+
+    .close:hover,
+    .close:focus {
+        color: black;
+        text-decoration: none;
+        cursor: pointer;
+    }
+</style>
 </head>
+
+<script type="text/javascript">
+    $(function() {
+        $("#update").click( function() {
+            $.ajax({
+                type: "GET",
+                url: "update.php"
+            });
+            alert("Database updated.");
+            // $().load("update.php", function(data){
+            //     alert("Database updated.");
+            // });
+        });
+    });
+
+</script>
+
+<body>
+
+    <div id="main" style="width:100%; display:inline-block; text-align:center;">
+
+        <div id="update">
+            <button>Update Database</button>
+        </div>
+
+        <div>
+            <!-- Trigger/Open The Modal -->
+            <button id="additem">Add Item</button>
+
+            <!-- The Modal -->
+            <div id="myModal" class="modal">
+
+                <!-- Modal content -->
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <p>Some text in the Modal..</p>
+                </div>
+
+            </div>
+
+        </div>
+
+        <script type="text/javascript">    
+            // Get the modal
+            var modal = document.getElementById('myModal');
+
+            // Get the button that opens the modal
+            var btn = document.getElementById("additem");
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks on the button, open the modal 
+            btn.onclick = function() {
+                modal.style.display = "block";
+            }
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        </script>
 
 
 <?
@@ -16,14 +124,42 @@ $password = 'userDBpasshere';
 $schema = 'DBName';
 $apiKey = 'yourBlizzardApiKey';
 $realmName = 'YourRealmName';
-$realmRegion = 'YourRealRegion';
+$realmRegion = 'YourRealRegion'
 
 $conn = new mysqli($servername, $username, $password, $schema); $conn->set_charset('utf8');
 if ($conn->connect_error) { die('Connection failed: ' . $conn->connect_error); }
 
 
-// in this part i'm making all SQL queries
+// Dynamic DB Queries:
+// SELECT from entries database and save as array
+// then loop through entries to get prices
 
+$list = array();
+$query = "SELECT * from entries";
+$result = $conn->query($query);
+while($row = $result->fetch_assoc()) {
+    $list[] = $row;
+};
+
+foreach($list as $item) {
+    $id = $item['item'];
+    $amount = $item['amount'];
+    $query = "SELECT buyout/10000 as MIN, quantity as QTY FROM auctions where item = '$id' ORDER BY buyout ASC LIMIT 1";
+    $result = $conn->query($query);
+
+    if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $lowest = floatval($row["MIN"]);
+            $qty = intval($row["QTY"]);
+            $final = $lowest / $qty;
+        };
+        echo $final;
+    } else {
+        echo "0 results";
+    };
+};
+
+// in this part i'm making all SQL queries
 
 $sql_herb1 = "SELECT MIN(buyout)/10000/200 as MIN FROM auctions where item=124105 and quantity=200"; //Starlight Rose cheapest price x200 stack
 $result_herb1 = $conn->query($sql_herb1);
@@ -32,7 +168,7 @@ if ($result_herb1->num_rows > 0) {
     // output data of each row
     while($row = $result_herb1->fetch_assoc()) {
 
-	 $Starlight_Rose=$row["MIN"];
+     $Starlight_Rose=$row["MIN"];
 
     }
 } else {
@@ -46,7 +182,7 @@ if ($result_herb2->num_rows > 0) {
     // output data of each row
     while($row = $result_herb2->fetch_assoc()) {
 
-	 $Fjarnskaggl=$row["MIN"];
+     $Fjarnskaggl=$row["MIN"];
 
     }
 } else {
@@ -54,14 +190,14 @@ if ($result_herb2->num_rows > 0) {
 }
 
 
-$sql_herb3 = "SELECT MIN(buyout)/10000/200 as MIN FROM auctions where item=124102 and quantity=200"; //Fjarnskaggl cheapest price x200 stack
+$sql_herb3 = "SELECT MIN(buyout)/10000/200 as MIN FROM auctions where item=72092"; //Fjarnskaggl cheapest price x200 stack
 $result_herb3 = $conn->query($sql_herb3);
 
 if ($result_herb3->num_rows > 0) {
     // output data of each row
     while($row = $result_herb3->fetch_assoc()) {
 
-	 $Dreamleaf=$row["MIN"];
+     $Dreamleaf=$row["MIN"];
 
     }
 } else {
@@ -76,7 +212,7 @@ if ($result_flask1->num_rows > 0) {
     // output data of each row
     while($row = $result_flask1->fetch_assoc()) {
 
-	 $Wispered_Pact=$row["MIN"];
+     $Wispered_Pact=$row["MIN"];
          $Wispered_Last_Seller=$row["owner"];
 
     }
@@ -91,7 +227,7 @@ if ($result_flask2->num_rows > 0) {
     // output data of each row
     while($row = $result_flask2->fetch_assoc()) {
 
-	 $Wispered_Pact_Q=$row["SUM"];
+     $Wispered_Pact_Q=$row["SUM"];
 
 
     }
@@ -151,7 +287,7 @@ a, u {
 <col style="width: 120px">
 </colgroup>
   <tr>
-  	<th class="tg-9nbt">!</th>
+    <th class="tg-9nbt">!</th>
     <th class="tg-9nbt">Item name:</th>
     <th class="tg-9right">Stack:</th>
     <th class="tg-9right">Low buy:</th>
@@ -165,8 +301,13 @@ a, u {
 <tr> <td>
 
 <? if ($Wisper_Profit>10) {if ($Wispered_Last_Seller=="Yoshyoka") echo '<img src="2.gif"'; else echo '<img src="1.gif"';};?></td>
-<td><a href="//www.wowhead.com/item=127847" class="q3" rel="gems=23121&amp;ench=2647&amp;pcs=25695:25696:25697"></td><td align="right">1</td><td align="right"><? echo round($Wispered_Pact,2)?><img src="gold.png"><td align="right"><? echo round($Wispered_Pact,2) ?><img src="gold.png"><td align="right"><? echo round($Wisper_Crafting_Cost,2). '<img src="gold.png">'?></td><td align="right"><? echo $Wispered_Pact_Q?></td><td align="right"><? if ($Wisper_Profit>0)  echo '<b><font color=green> +' .round($Wisper_Profit,2).'<img src="gold.png">'; else echo '<b><font color=red>' .round($Wisper_Profit,2).'<img src="gold.png">' ?><td><? if ($Wispered_Last_Seller==Yoshyoka) echo "<font color=green>" . $Wispered_Last_Seller; else echo "<font color=red>".$Wispered_Last_Seller ?></td></td>
+<td><a href="//www.wowhead.com/item=127847" class="q3" rel="gems=23121&amp;ench=2647&amp;pcs=25695:25696:25697"></td>
+<td align="right">1</td><td align="right"><? echo round($Wispered_Pact,2)?><img src="gold.png"><td align="right"><? echo round($Wispered_Pact,2) ?><img src="gold.png"><td align="right"><? echo round($Wisper_Crafting_Cost,2). '<img src="gold.png">'?></td>
+<td align="right"><? echo $Wispered_Pact_Q?></td>
+<td align="right"><? if ($Wisper_Profit>0)  echo '<b><font color=green> +' .round($Wisper_Profit,2).'<img src="gold.png">'; else echo '<b><font color=red>' .round($Wisper_Profit,2).'<img src="gold.png">' ?><td><? if ($Wispered_Last_Seller==Yoshyoka) echo "<font color=green>" . $Wispered_Last_Seller; else echo "<font color=red>".$Wispered_Last_Seller ?></td></td>
 
 
 </table>
 
+    </div>
+</body>
--- update.php
+++ update.php
@@ -32,30 +32,24 @@ if ($result3->num_rows > 0) {
 	$lastentry=$row["realm"];
 
 
-if ($lastentry==$responseObject['files'][0]['lastModified'])
-{
-echo '<br>';
+if ($lastentry < $responseObject['files'][0]['lastModified']) {
+	$conn ->query("TRUNCATE TABLE auctions");
+	$conn ->query("TRUNCATE TABLE status");
 
-}
+	$response = file_get_contents('https://'. $realmRegion .'.api.battle.net/wow/auction/data/'. $realmName .'?locale=en_GB&apikey=' . $apiKey); $responseObject = json_decode($response, true);
+	$a=json_decode($response,true);
+	$sql = "INSERT INTO status (realm) VALUES(".$responseObject['files'][0]['lastModified'].");";
+	$conn->query($sql);
 
-  else          {
-		$conn ->query("TRUNCATE TABLE auctions");
-		$conn ->query("TRUNCATE TABLE status");
-
-		$response = file_get_contents('https://'. $realmRegion .'.api.battle.net/wow/auction/data/'. $realmName .'?locale=en_GB&apikey=' . $apiKey); $responseObject = json_decode($response, true);
-		$a=json_decode($response,true);
-		$sql = "INSERT INTO status (realm) VALUES(".$responseObject['files'][0]['lastModified'].");";
-		$conn->query($sql);
-
-		$response = file_get_contents('https://'. $realmRegion .'.api.battle.net/wow/auction/data/'. $realmName .'?locale=en_GB&apikey=' . $apiKey); $responseObject = json_decode($response, true);
-		$auctionsFile = file_get_contents($responseObject['files'][0]['url']); $auctionsArray = json_decode($auctionsFile, true)['auctions'];
-		foreach ($auctionsArray as $auction) { $sql = "INSERT INTO auctions (auc, item, owner, buyout, quantity) VALUES(" . $auction['auc'].",". $auction['item'].",'".$auction['owner']."',".$auction['buyout'].",".$auction['quantity'].");"; $conn->query($sql); }
-		$conn ->query("delete from auctions where buyout=0");
-
-
-		}
+	$response = file_get_contents('https://'. $realmRegion .'.api.battle.net/wow/auction/data/'. $realmName .'?locale=en_GB&apikey=' . $apiKey); $responseObject = json_decode($response, true);
+	$auctionsFile = file_get_contents($responseObject['files'][0]['url']); $auctionsArray = json_decode($auctionsFile, true)['auctions'];
+	foreach ($auctionsArray as $auction) { $sql = "INSERT INTO auctions (auc, item, owner, buyout, quantity) VALUES(" . $auction['auc'].",". $auction['item'].",'".$auction['owner']."',".$auction['buyout'].",".$auction['quantity'].");"; $conn->query($sql); }
+	$conn ->query("delete from auctions where buyout=0");
 
 } else {
-    echo "0 results";
-}
+
+	echo '<br>';
+
+	}
+};
 ?>
